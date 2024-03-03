b3edb6a96c8324a2c14da50baaf9ccee332f74e3
Alex Tiger <alexanddanik@gmail.com>
2024-03-02 23:10:03 -0500

    dig

--- RestApiWithLambda/MinimalLambda/Program.cs
+++ RestApiWithLambda/MinimalLambda/Program.cs
@@ -3,8 +3,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
-// Add AWS Lambda support. When application is run in Lambda Kestrel is swapped out as the web server with Amazon.Lambda.AspNetCoreServer. This
-// package will act as the webserver translating request and responses between the Lambda event source and ASP.NET Core.
+// Add AWS Lambda support.
+// When application is run in Lambda Kestrel is swapped out as the web server
+// with Amazon.Lambda.AspNetCoreServer.
+// This package will act as the webserver translating request and responses
+// between the Lambda event source and ASP.NET Core.
 builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
 
 var app = builder.Build();
